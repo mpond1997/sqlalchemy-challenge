8b93c87897905ab91b620969a2ae8d6c798e91e0
mpond1997 <michaelpond@Michaels-MacBook-Pro.local>
2023-12-12 11:14:28 -0700

    Submitted Code

--- Surfs_Up/app.py
+++ Surfs_Up/app.py
@@ -1,6 +1,6 @@
 # Import the dependencies.
 from flask import Flask, jsonify
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 
@@ -40,27 +40,7 @@ def welcome():
         f"/api/v1.0/<start>/<end>"
     )
 @app.route("/api/v1.0/precipitation")
-def precipitation_api():
+def precipitation():
     session = Session(engine)
-    session.close()
 
-@app.route("/api/v1.0/stations")
-def stations_api():
-    session = Session()
-    all_stations = station.id(session)
-    session.close()
-    return jsonify({'stations': all_stations})
-
-
-@app.route("/api/v1.0/tobs")
-def tobs_api():
-    session = Session()
-    active_stations = session.query(measurement.station, func.count(measurement.station)).\
-        group_by(measurement.station).\
-        order_by(func.count(measurement.station).desc()).all()
-    session.close()
-    return jsonify({'tobs': active_stations})
-
-@app.route("/api/v1.0/<start>")
-def start_api():
-    
+    results = session.query(measurement.prcp)
